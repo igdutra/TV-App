c5ec61b57f943f300a3ef310e4cf4da15927159b
Ivo <ivogdutra@gmail.com>
2022-03-15 07:58:36 -0300

    Add image and title

--- TVApp/Scenes/ShowDetails/ShowDetailsViewController.swift
+++ TVApp/Scenes/ShowDetails/ShowDetailsViewController.swift
@@ -14,6 +14,12 @@ final class ShowDetailsViewController: UIViewController {
     
     // MARK: - UI Elements
     
+    private lazy var showImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.rowHeight = UITableView.automaticDimension
@@ -32,17 +38,25 @@ final class ShowDetailsViewController: UIViewController {
 // MARK: - ViewConfiguration
 extension ShowDetailsViewController: ViewConfiguration {
     func setupHierarchy() {
-        view.addSubviews([tableView])
+        view.addSubviews([showImageView, tableView])
     }
     
     func setupConstraints() {
-        tableView.edgesToSuperView(toSafeArea: true)
+        showImageView
+            .centerHorizontalToSuperView()
+            .heightTo(200)
+            .topToSuperview(4, toSafeArea: true)
+        
+        tableView
+            .edgesToSuperView(excluding: .top, toSafeArea: true)
+            .topToBottom(of: showImageView, margin: 24)
     }
     
     func setupConfigurations() {
         tableView.delegate = self
         tableView.dataSource = tableViewDataSource
         tableView.register(cellClass: UITableViewCell.self)
+        tableView.tableHeaderView = makeHeader()
     }
 }
 
@@ -54,8 +68,10 @@ extension ShowDetailsViewController: ShowDetailsViewControllerProtocol {
     }
     
     func display(showDetails: ShowDetails) {
-        // TODO: add missing labels and image from viewModel
         title = showDetails.name
+        // Improvement: This asset image could be in an script generated file, e.g. using SwiftGen
+        showImageView.from(url: showDetails.image, placeholder: UIImage(named: "imagePlaceholder"))
+        
     }
 }
 
@@ -63,5 +79,19 @@ extension ShowDetailsViewController: ShowDetailsViewControllerProtocol {
 extension ShowDetailsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // TODO: Go to Episode Detail
+    }
+}
+
+// MARK: - Private Methods
+private extension ShowDetailsViewController {
+    // Improvement: transform into component
+    func makeHeader() -> UIView {
+        let headerView: UIView = .init(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        let label: UILabel = .init(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+        label.text = "Episodes"
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        headerView.addSubview(label)
+        headerView.backgroundColor = .white
+        return headerView
     }
 }
